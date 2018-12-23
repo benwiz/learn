c680ffbcd1ba84b307f9f009a6812ceb5776387a
Ben Wiz <bwisialowski@gmail.com>
2018-12-23 15:22:52 -0800

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,68 @@
 const path = require("path");
 
-module.exports = {
-  mode: "none",
-  entry: "./src/index.ts",
-  devtool: "inline-source-map",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/
-      }
-    ]
+module.exports = [
+  {
+    entry: {
+      index: "./src/index.ts"
+    },
+    devtool: "inline-source-map",
+    mode: "none",
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/
+        }
+      ]
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"]
+    },
+    output: {
+      filename: "[name].js",
+      path: path.resolve(__dirname, "dist")
+    }
   },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"]
-  },
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
+  {
+    entry: {
+      index: "./src/index.scss"
+    },
+    // I guess it isn't necessary?
+    // output: {
+    //   // This is necessary for webpack to compile. But we never use it.
+    //   // filename: "tmp.js"
+    // },
+    mode: "none",
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: "[name].css"
+              }
+            },
+            { loader: "extract-loader" },
+            { loader: "css-loader" },
+            {
+              loader: "sass-loader",
+              options: {
+                importer(url, prev) {
+                  if (url.indexOf("@material") === 0) {
+                    const filePath = url.split("@material")[1];
+                    const nodeModulePath = `./node_modules/@material/${filePath}`;
+                    return { file: require("path").resolve(nodeModulePath) };
+                  }
+                  return { file: url };
+                }
+              }
+            }
+          ]
+        }
+      ]
+    }
   }
-};
+];
