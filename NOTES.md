d0e44478203cfc1bfbb2990c18d7f4186ca02779
Ben Wiz <bwisialowski@gmail.com>
2018-12-31 09:47:09 -0800

    npm watch works

--- NOTES.md
+++ NOTES.md
@@ -28,6 +28,5 @@ emcc test.c -s WASM=1 -s "EXTRA_EXPORTED_RUNTIME_METHODS=['ccall', 'cwrap']"
 
 ## To Do
 
-- Now that I have a working hello world, try deploying to NPM
-- Once NPM deploy works, set up the html, javascript, webpack, etc.
+
 - Then figure out how to set up game loop using https://medium.com/@mbebenita/lets-write-pong-in-webassembly-ac3a8e7c4591
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import WebAssembly from 'webassembly';
 
-console.log('Go: 3');
+// The path to the wasm binary. I need to figure out how to make it work with webpack bundler.
+const wasmPath = './node_modules/@benwiz/boba.wasm/dist/boba.wasm';
 
-WebAssembly.load('./node_modules/@benwiz/boba.wasm/dist/boba.wasm').then((module) => {
-  console.log(`1 + 2 + 3 = ${module.exports.add(1, 2, 3)}`);
-});
+const run = (module) => {
+  console.log(`SUM = ${module.exports.add(1, 2, 3)}`);
+};
+
+// Load the wasm
+WebAssembly.load(wasmPath).then(run);
