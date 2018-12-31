400ca1338e1b2cea0b963601569fede82d1eb76d
Ben Wiz <bwisialowski@gmail.com>
2018-12-31 08:44:08 -0800

    starting over

--- wasm2cb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Usage: ./wasm2cb.sh <filename>.wasm
-out=""
-for x in $(xxd -ps -c1 $1)
-  do out="$out,$(( 16#$x ))"
-done
-echo $out|sed "s/^,\(.*\)$/var wasmCode = new Uint8Array([\1]);/"
