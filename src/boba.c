3b4b9e67f5d32d57287b7edbc94795e98ac33f4d
Ben Wiz <bwisialowski@gmail.com>
2018-12-31 16:17:56 -0800

    prep c code

--- src/boba.c
+++ src/boba.c
@@ -1,30 +1,55 @@
-// program.c
 #include <webassembly.h>
 
+//
+// External JavaScript functions
+//
+extern void jsSetInterval(void (*callback)());
+extern void jsDrawVertex(int x, int y);
+extern void jsClearCanvas();
+
+//
+// Structs
+//
 struct Vertex
 {
     int x;
     int y;
 };
 
-extern void jsSetInterval(void (*callback)());
-extern void jsDrawVertex(int x, int y);
-extern void jsClearCanvas();
+//
+// Setup functions
+//
 
+//
+// Update functions
+//
+
+//
+// Draw functions
+//
+
+//
+// Loop functions
+//
+// tick executes the drawing and updating functions
 void tick()
 {
     console_log("c.tick");
     jsClearCanvas();
 }
 
+// runCallback executes `tick` and is the entry point into the main loop
 export int runCallback(void (*callback)())
 {
-    console_log("c.runCallback");
     callback();
 }
 
+// start sets up vertices and begins the main loop
 export void start()
 {
-    console_log("c.start");
+    // Call setup functions
+    // TODO: Set up vertices
+
+    // Start loop
     jsSetInterval(tick);
 }
